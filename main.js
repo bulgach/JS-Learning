4fafb5ca744a546079be0aab4528e051815966a3
bulgach <maksimbulgach@gmail.com>
2023-03-04 11:13:34 +0300

    Присвоение

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-console.log('bulgach')
-console.dir('good' + 'morning')
+console.log('bulgach');
+console.dir('good' + 'morning');
 
+let a = 'salam';
+a = a + 'aleikum';
+console.log(a);
+const b = 20;
+//нельяза присваивать новые знаечения const
+//assigment to const variable
+//b = 10;
+console.log(b);
  
